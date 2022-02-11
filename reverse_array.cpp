495fba595282ea27f321b3976fb738c56d97f06d
indal saroj <indal.saroj1@delhivery.com>
2022-02-11 16:31:47 +0530

    [array_reverse]

---
+++ reverse_array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n, int start)
+{
+    if(start>=n)
+        return;
+    int temp=arr[start];
+    arr[start]=arr[n];
+    arr[n]=temp;
+
+
+    reverse(arr,n-1,start+1);
+}
+int main()
+{
+    int s;
+    scanf("%d",&s);
+    int a[s];
+    for(int i=0;i<s;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    reverse(a,s-1,0);
+    for(int i=0;i<s;i++)
+    {
+        // printf("%d",&a[i]);
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
